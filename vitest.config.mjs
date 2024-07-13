70e737f69a4962cddf22b40710b1ef61313b2565
lucaoskaique <lucas.kaique175@hotmail.com>
2024-07-13 18:46:28 -0300

    Initial Commit 🤞

---
+++ vitest.config.mjs
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react';
+import { config } from 'dotenv';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+config();
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    fileParallelism: false,
+    isolate: false,
+    testTimeout: 60_000,
+    hookTimeout: 30_000,
+    setupFiles: ['tests/setup.js'],
+  },
+});
