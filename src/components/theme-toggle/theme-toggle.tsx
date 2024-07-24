3745b847394ecf7e3b597f12aa95676dad2aa28b
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-07-24 19:17:36 -0300

    refactor: add useEffect to data-mode

--- src/components/theme-toggle/theme-toggle.tsx
+++ src/components/theme-toggle/theme-toggle.tsx
@@ -1,9 +1,17 @@
 "use client"
 
 import { useTheme } from "next-themes"
+import { useEffect } from "react"
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-mode",
+      theme === "dark" ? "dark" : "light"
+    )
+  }, [theme])
 
   const handleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark")
