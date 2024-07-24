bae737c6ee7b80cc311584cc32003ddd19ccc634
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-07-24 12:55:18 -0300

    refactor: removed useEffect hook

--- src/components/theme-toggle/theme-toggle.tsx
+++ src/components/theme-toggle/theme-toggle.tsx
@@ -1,20 +1,12 @@
 "use client"
 
 import { useTheme } from "next-themes"
-import { useEffect } from "react"
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme()
 
-  useEffect(() => {
-    document.documentElement.setAttribute(
-      "data-mode",
-      theme === "dark" ? "dark" : "light"
-    )
-  }, [theme])
-
   const handleTheme = () => {
-    theme === "dark" ? setTheme("light") : setTheme("dark")
+    setTheme(theme === "dark" ? "light" : "dark")
   }
 
   return <button onClick={handleTheme}>Toggle Theme</button>
