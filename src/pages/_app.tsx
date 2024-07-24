f6f5af6dcdbc602150db0b08ca5d74a1ba9fdb33
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-07-24 11:07:07 -0300

    feat: add theme-provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+
+import { ThemeProvider } from "@/contexts/theme-provider"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
