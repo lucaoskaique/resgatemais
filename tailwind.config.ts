2f0b80f3aebfc9c9207c1172e5603e28c40f0947
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-07-29 13:07:33 -0300

    feat: add backgroundImage pattern

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,8 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero-pattern": "url('/hero-bg.png')"
       }
     }
   },
