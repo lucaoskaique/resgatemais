e41ab8c75e928d9d331d63eacca29e946a3ea661
Victor Feitosa Garcia <vfeitosa.garcia@gmail.com>
2024-07-29 15:47:04 -0300

    12 identificação de tema tailwind (#15)
    * feat: add palette color
    
    * refactor: remove useEffect hook
    
    * refactor: add parameters and ThemeByClassName
    
    * fix: add @layer directive to theme palette
    
    * refactor: remove commented code

--- src/components/theme-toggle/theme-toggle.tsx
+++ src/components/theme-toggle/theme-toggle.tsx
@@ -1,17 +1,9 @@
 "use client"
 
 import { useTheme } from "next-themes"
-import { useEffect } from "react"
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme()
-
-  useEffect(() => {
-    document.documentElement.setAttribute(
-      "data-mode",
-      theme === "dark" ? "dark" : "light"
-    )
-  }, [theme])
 
   const handleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark")
