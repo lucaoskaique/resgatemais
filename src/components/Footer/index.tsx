27d2b39dfdf6a4062525167341af58c0ccdf213e
Gabriel Albuquerque <146096688+gabriel-chagas-albuquerque@users.noreply.github.com>
2024-08-05 22:17:36 -0300

    Footer Component criado (#19)
    * --feat Footer Component
    
    * --feat Footer Component
    
    * --Fix correction of errors in the code
    
    * --fix adjusts in Footer Component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,39 @@
+export interface FooterProps {
+  textFooter?: string
+  title?: string
+  phone?: string
+  email?: string
+  instagram?: string
+}
+
+const Footer = ({
+  textFooter,
+  title,
+  phone,
+  email,
+  instagram
+}: FooterProps) => {
+  return (
+    <div className="flex h-[367px] w-full flex-col items-center justify-around bg-red-500 text-white md:flex-row">
+      <p
+        className={`max-w-[264px] text-sm font-bold md:max-w-[383px] md:text-lg`}>
+        {textFooter}
+      </p>
+
+      <div className="flex min-w-[211px] flex-col">
+        <h2 className={`mb-[33px] text-[18px] font-bold leading-[18px]`}>
+          {title}
+        </h2>
+        <ul className={`text-sm`}>
+          {email && <li className="leading-[16.24px]">E-mail: {email}</li>}
+          {instagram && (
+            <li className="leading-[16.24px]">Instagram: {instagram}</li>
+          )}
+          {phone && <li className="leading-[16.24px]">Telefone: {phone}</li>}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
