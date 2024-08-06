9bf7eb78f1caa9dccce17dc2458ccf6b55c1c3ac
Gabriel Albuquerque <gabrieldafortaleza0100@gmail.com>
2024-08-06 13:10:09 -0300

    --fix troca da tag img pela tag Image do next --fix correção do responsivo para mobile

--- src/components/Register-Email/index.tsx
+++ src/components/Register-Email/index.tsx
@@ -20,16 +20,18 @@ const RegisterEmail = ({ title, textLogo }: RegisterEmailProps) => {
           id="email-cadastro"
         />
 
-        <div className="flex flex-row items-center gap-[30px]">
+        <div className="flex flex-row items-center gap-[15px] sm:gap-[30px]">
           <Image
             className="ml-[3px] mt-[32px]"
             src="./logo-white-red.png"
             alt="Logo Resgaste Mais"
+            width={100}
+            height={100}
           />
 
           {textLogo && (
             <h3
-              className={`mt-[30px] w-[230px] text-[18px] font-bold leading-[20.88px] text-white`}>
+              className={`mt-[30px] w-[180px] text-[14px] font-bold leading-[20.88px] text-white sm:w-[230px] sm:text-[18px]`}>
               {textLogo}
             </h3>
           )}
