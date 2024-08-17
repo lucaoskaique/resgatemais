ecfe2a6c57f0b9a46bcaf00147ef43a2f3a6c907
lucaoskaique <lucas.kaique175@hotmail.com>
2024-08-17 19:07:12 -0300

    feat: Add Home template component
    The `Home` template component is added to provide the structure for the home page of the application. It includes different sections such as hero, about, mission, donations, and newsletter. This component will be used as a template for the home page content.
    
    Co-authored-by: lucaoskaique <lucas.kaique175@hotmail.com>
    Co-authored-by: Victor Feitosa Garcia <vfeitosa.garcia@gmail.com>

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,26 @@
+import Base from "../Base"
+
+const Home = () => {
+  return (
+    <Base>
+      <section style={{ height: "100px", backgroundColor: "lightgray" }}>
+        {/* sessão hero */}
+      </section>
+      <section style={{ height: "100px", backgroundColor: "lightblue" }}>
+        {/* sessão about */}
+      </section>
+      <section style={{ height: "100px", backgroundColor: "lightgreen" }}>
+        {/* sessão mission (Missão, Visão e Valores) */}
+      </section>
+      <section style={{ height: "100px", backgroundColor: "lightcoral" }}>
+        {/* sessão donations (Doações Mobilizadas) */}
+      </section>
+      <section
+        style={{ height: "100px", backgroundColor: "lightgoldenrodyellow" }}>
+        {/* sessão newsletter */}
+      </section>
+    </Base>
+  )
+}
+
+export default Home
