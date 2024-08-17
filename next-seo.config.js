66f14dc44dcf95d8362944c6de7682a359ece79f
lucaoskaique <lucas.kaique175@hotmail.com>
2024-08-17 19:07:58 -0300

    feat: Add lazy loading for images and update next-seo.config.js
    The code changes include adding lazy loading for images to improve page load performance. Additionally, the next-seo.config.js file is updated to include Open Graph and Twitter metadata for better social media sharing.
    
    Co-authored-by: lucaoskaique <lucas.kaique175@hotmail.com>
    Co-authored-by: Victor Feitosa Garcia <vfeitosa.garcia@gmail.com>

---
+++ next-seo.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  openGraph: {
+    type: "website",
+    locale: "pt-BR",
+    url: "https://www.resgatemais.com.br",
+    site_name: "Resgate+",
+    title: "Organização Audiovisual da Sociedade Civil",
+    images: [
+      {
+        url: "https://www.resgatemais.com.br/images/logo.png",
+        width: 340,
+        height: 80,
+        alt: "Organização Audiovisual da Sociedade Civil"
+      }
+    ]
+  },
+  twitter: {
+    handle: "@resgate.mais",
+    site: "@site",
+    cardType: "summary_large_image"
+  }
+}
