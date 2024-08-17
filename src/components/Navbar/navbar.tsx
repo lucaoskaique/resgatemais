33ecb18345c48d82bd39f6b98a03cda6f7db1f6a
Lucas Kaíque <lucas.kaique175@hotmail.com>
2024-08-17 17:14:41 -0300

    feat: Update Storybook and fix import (#33)
    * feat: updated storybook and fix import
    
    * feat: added codeowners file

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 
-import { SwitchTheme } from "../switch-theme/switch-theme"
+import { SwitchTheme } from "../switchTheme/switch-theme"
 
 interface Links {
   dark?: string
