1b8104dbc51d078e8eaed13a579f02e2f8c4e139
carlosvfb <cacavfb2107@gmail.com>
2024-08-22 15:27:02 -0300

    feat: adicionando o componente drawer

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,10 +2,12 @@ import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 
+import { Drawer } from "../Drawer"
 import { SwitchTheme } from "../switchTheme/switch-theme"
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
+
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
@@ -122,42 +124,7 @@ export default function Navbar() {
             />
           </button>
         </div>
-        <nav
-          className={`fixed left-0 top-0 z-20 h-full w-full bg-white text-black transition-transform duration-300 ease-in-out dark:bg-[#323232] dark:text-white ${isOpen ? "translate-y-0 transform" : "-translate-y-full transform"}`}
-          role="navigation">
-          <button
-            onClick={toggleMenu}
-            className="absolute right-4 top-4 text-3xl font-bold text-black dark:text-white"
-            aria-label="Close Menu">
-            &times;
-          </button>
-          <ul className="flex h-full flex-col items-center justify-center space-y-6 py-4">
-            <li>
-              <Link
-                href="#"
-                className="block px-4 py-2 text-lg"
-                data-testid="sobre-link-small">
-                {textLinks[0]}
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="block px-4 py-2 text-lg"
-                data-testid="acoes-link-small">
-                {textLinks[1]}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="block rounded-[0.313rem] bg-color-red px-4 py-3 text-lg"
-                data-testid="doar-link-small"
-                href="#">
-                {textLinks[2]}
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <Drawer isOpen={isOpen} toggleMenu={toggleMenu} />
       </nav>
     </header>
   )
