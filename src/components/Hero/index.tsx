df4ef867758e14a4a91d45bc5a4aa72979d0ef04
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-08-22 15:08:56 -0300

    refactor: add props and hooks

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,32 +1,81 @@
-import Image from "next/image"
+import Image, { StaticImageData } from "next/image"
+import { useRouter } from "next/router"
 
-export type HeroProps = {
+import { cn } from "@/lib/utils"
+
+type DonatePageProps = {
   title?: string
-  logo?: string
-  imageHeroUrl?: string
+  paragraph1?: string
+  paragraph2?: string
+  donateCall?: string
+  qrCode?: string | StaticImageData
 }
 
-export default function Hero({ title, logo, imageHeroUrl }: HeroProps) {
+type HomeProps = {
+  logo?: string
+  title?: string
+}
+
+export type HeroProps = {
+  bgHero?: string | StaticImageData
+  home?: HomeProps
+  donatePage?: DonatePageProps
+}
+
+export default function Hero({ bgHero, home, donatePage }: HeroProps) {
+  const router = useRouter()
+  const donatePageRoute = router.asPath === "/donate"
+  const bgHeroUrl = typeof bgHero === "string" ? bgHero : bgHero?.src
+
   return (
-    <div className="relative">
-      <div className="custom-background h-screen w-full">
-        <div className="absolute bottom-[50px] left-[50px] text-white md:bottom-[110px] md:left-[150px]">
-          <Image
-            src={logo || "/icon-red.png"}
-            alt="A+Vermelho"
-            width={73}
-            height={73}
-          />
-          <h1 className="max-w-52 pt-8 text-2xl font-bold md:text-3xl lg:max-w-80 lg:text-4xl">
-            {title || "Organização Audiovisual da Sociedade Civil"}
-          </h1>
-        </div>
-        <style jsx>{`
-          .custom-background {
-            background: url(${imageHeroUrl}) center/cover no-repeat;
-          }
-        `}</style>
+    <section
+      className={cn(
+        donatePageRoute ? "grid-rows-2" : "grid-rows-1 items-center",
+        "custom-bg grid h-screen w-full justify-items-center gap-y-16 pb-16 pt-32 text-white lg:grid-cols-2 lg:grid-rows-1 lg:items-center lg:justify-items-start"
+      )}>
+      <div className="max-w-[480px] text-center lg:ml-20 lg:text-left">
+        {donatePageRoute ? (
+          <>
+            <h1 className="text-lg font-bold text-highlight">
+              {donatePage?.title}
+            </h1>
+            <p className="mt-10">
+              {donatePage?.paragraph1}
+              <span className="block lg:mt-4">{donatePage?.paragraph2}</span>
+            </p>
+          </>
+        ) : (
+          <>
+            {home?.logo && (
+              <Image src={home?.logo} alt="" width={73} height={73} />
+            )}
+            <h1 className="max-w-52 pt-8 text-2xl font-bold md:text-3xl lg:max-w-80 lg:text-4xl">
+              {home?.title}
+            </h1>
+          </>
+        )}
       </div>
-    </div>
+      {donatePageRoute && (
+        <aside className="lg:justify-self-end lg:pr-[120px]">
+          <p className="mb-4 text-center text-xs font-bold text-white">
+            {donatePage?.donateCall}
+          </p>
+          {donatePage?.qrCode && (
+            <Image
+              className="size-[180px]"
+              src={donatePage?.qrCode}
+              alt="QR Code"
+              width={180}
+              height={180}
+            />
+          )}
+        </aside>
+      )}
+      <style jsx>{`
+        .custom-bg {
+          background: url(${bgHeroUrl}) center/cover no-repeat;
+        }
+      `}</style>
+    </section>
   )
 }
