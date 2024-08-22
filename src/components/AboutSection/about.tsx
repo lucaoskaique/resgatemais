a07f11870b2a7b93fccb261e14388f67efe33e5d
Livia Giro <lgmayrinck@gmail.com>
2024-08-22 12:15:49 -0300

    criando section about e card component

---
+++ src/components/AboutSection/about.tsx
@@ -0,0 +1,33 @@
+// //import { StaticImageData } from "next/image"
+
+// export type AboutProps = {
+//   backgroundColor?: string
+//   title?: string
+//   paragraph1?: string
+// }
+
+// export default function About({
+//   backgroundColor,
+//   title,
+//   paragraph1,
+// }: AboutProps) {
+//   return (
+//     <section
+//       className={`h-screen grid w-full grid-rows-2 justify-items-center gap-y-16 py-20 lg:grid-cols-2 lg:grid-rows-1 lg:items-center lg:justify-items-start
+//         relative`}
+//     >
+//       <div
+//         className={`bg-[#323232] h-[50vh] absolute top-0 left-0 w-full h-[50%]`}
+//       />
+//       <div
+//         className={`bg-[#D63E44] h-[50vh] absolute top-[50%] left-0 w-full h-[50%]`}
+//       />
+//       {backgroundColor}
+
+//       <div className="h - [50vh] absolute top-0 left-0 w-full h-[50%]">
+//       <h2 className="text-lg font-bold leading-none text-left text-red pb-40">{title}</h2>
+//       <p className="text-lg text-white">{paragraph1}</p>
+//       </div>
+//     </section>
+//     )
+// }
---
+++ src/components/AboutSection/stories.tsx
@@ -0,0 +1,17 @@
+// import { Meta, StoryObj } from "@storybook/react"
+
+// import AboutSection, { AboutProps } from './AboutSection';
+
+// export default {
+//     title: "Resgatemais/About",
+//     component: AboutSection,
+//     argTypes: {
+//       title: { control: 'text' },
+//       paragraph1: { control: 'text' },
+//     },
+//   parameters: {
+//     layout: "fullscreen"
+//   }
+// } as Meta<AboutProps>
+
+// export const Default: StoryObj = {}
