ffd81d2c4305f96c41719ba12c15dbd8bef28e3e
lucaoskaique <lucas.kaique175@hotmail.com>
2024-08-22 14:21:06 -0300

    feat: Update Navbar component with dropdown drawer in mobile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react"
@@ -125,23 +123,37 @@ export default function Navbar() {
           </button>
         </div>
         <nav
-          className={`absolute right-0 top-7 mt-12 w-48 bg-white text-black dark:bg-[#323232] dark:text-white lg:hidden ${isOpen ? "" : "hidden"}`}
+          className={`fixed left-0 top-0 z-20 h-full w-full bg-white text-black transition-transform duration-300 ease-in-out dark:bg-[#323232] dark:text-white ${isOpen ? "translate-y-0 transform" : "-translate-y-full transform"}`}
           role="navigation">
-          <ul className="flex flex-col items-center py-4">
-            <li className="py-2">
+          <button
+            onClick={toggleMenu}
+            className="absolute right-4 top-4 text-3xl font-bold text-black dark:text-white"
+            aria-label="Close Menu">
+            &times;
+          </button>
+          <ul className="flex h-full flex-col items-center justify-center space-y-6 py-4">
+            <li>
               <Link
                 href="#"
-                className="block px-4 py-2"
+                className="block px-4 py-2 text-lg"
                 data-testid="sobre-link-small">
                 {textLinks[0]}
               </Link>
             </li>
-            <li className="py-2">
+            <li>
               <Link
                 href="#"
-                className="block px-4 py-2"
+                className="block px-4 py-2 text-lg"
                 data-testid="acoes-link-small">
                 {textLinks[1]}
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block rounded-[0.313rem] bg-color-red px-4 py-3 text-lg"
+                data-testid="doar-link-small"
+                href="#">
+                {textLinks[2]}
               </Link>
             </li>
           </ul>
