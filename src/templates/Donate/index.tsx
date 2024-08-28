3b337fcc7cd9e0dd1435251a4a07d5c7a144d47a
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-08-28 11:56:27 -0300

    remove: duplicated component

--- src/templates/Donate/index.tsx
+++ src/templates/Donate/index.tsx
@@ -1,23 +1,11 @@
 import DonateInfo from "@/components/DonateInfo"
 import { infoTemplate as Infotemp } from "@/components/DonateInfo/template"
-import Hero from "@/components/Hero"
-import { heroTemplate as Herotemp } from "@/components/Hero/stories"
 
 import Base from "../Base"
 
 const Donate = () => {
   return (
     <Base>
-      <Hero
-        bgUrl={Herotemp.bgHero.src}
-        donatePage={{
-          title: Herotemp.donatePage.title,
-          paragraph1: Herotemp.donatePage.paragraph1,
-          paragraph2: Herotemp.donatePage.paragraph2,
-          donateCall: Herotemp.donatePage.donateCall,
-          qrCode: Herotemp.donatePage.qrCode
-        }}
-      />
       <DonateInfo
         title={Infotemp.title}
         info={Infotemp.info}
