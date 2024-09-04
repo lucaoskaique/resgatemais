e7796cf675a038f9b7aaa7d75c99d1cd29b6bff9
lucaoskaique <lucas.kaique175@hotmail.com>
2024-09-04 15:05:42 -0300

    refactor: update Banner component link to /contact

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -7,7 +7,7 @@ export function Banner() {
     <div className="relative top-0 border-b border-white/10 bg-black py-3 text-white md:py-0">
       <div className="container flex flex-col items-center justify-center gap-4 md:h-12 md:flex-row">
         <Link
-          href="/"
+          href="/contact"
           target="_blank"
           className="group inline-flex items-center justify-center text-center text-sm leading-loose">
           ✨
