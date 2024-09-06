1e7129096ec29ff994337cb29176f0479a570df7
lucaoskaique <lucas.kaique175@hotmail.com>
2024-09-05 21:00:17 -0300

    Refactor next-seo.config.js to update image URLs

--- next-seo.config.js
+++ next-seo.config.js
@@ -15,26 +15,19 @@ export default {
       "Organização Audiovisual da Sociedade Civil dedicada a projetos audiovisuais e culturais.",
     images: [
       {
-        url: "https://resgatemais.vercel.app/images/bg-hero.png",
+        url: "https://resgatemais.org/images/bg-hero.png",
         width: 1200,
         height: 630,
         alt: "Resgate+ | Organização Audiovisual da Sociedade Civil",
         type: "image/jpeg"
-      },
-      {
-        url: "https://resgatemais.vercel.app/android-chrome-192x192.png",
-        width: 1200,
-        height: 630,
-        alt: "Resgate+ | Organização Audiovisual da Sociedade Civil",
-        type: "image/jpeg"
-      },
-      {
-        url: "https://resgatemais.vercel.app/android-chrome-192x192.png",
-        width: 340,
-        height: 80,
-        alt: "Resgate+ Logo",
-        type: "image/png"
       }
+      // {
+      //   url: "https://resgatemais.org/android-chrome-192x192.png",
+      //   width: 340,
+      //   height: 80,
+      //   alt: "Resgate+ | Organização Audiovisual da Sociedade Civil",
+      //   type: "image/png"
+      // }
     ]
   },
   twitter: {
